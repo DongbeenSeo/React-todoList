a35b69c162ab68215d87ad6b9b9f39bb00e4569b
DongbeenSeo <puus1021@gmail.com>
2018-06-07 15:39:08 +0900

    edit todos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React, { Component } from "react";
 
-const todos = [
-  {
-    id: 1,
-    body: "React study",
-    complete: true
-  },
-  {
-    id: 2,
-    body: "Redux study",
-    complete: false
-  }
-];
+//todos의 complete는 변경해야 하는 상태임으로 새로 coding
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        body: "React study",
+        complete: true
+      },
+      {
+        id: 2,
+        body: "Redux study",
+        complete: false
+      }
+    ]
+  };
   render() {
+    const { todos } = this.state;
     return (
       <div>
         <h1>TodoList</h1>
-        <ul>{todos.map(todo => <li key={todo.id}>{todo.body}</li>)}</ul>
+        <ul>
+          {todos.map(todo => (
+            <li className={todo.complete ? "complete" : ""} key={todo.id}>
+              {todo.body}
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
