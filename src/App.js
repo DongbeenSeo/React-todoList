6e122bdef89c0283621e2813217918dbe2e01da0
DongbeenSeo <puus1021@gmail.com>
2018-06-07 15:57:05 +0900

    할일 추가 기능

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React, { Component } from "react";
 
+// todos= [
+//   {
+//     id: 1,
+//     body: "React study",
+//     complete: true
+//   },
+//   {
+//     id: 2,
+//     body: "Redux study",
+//     complete: false
+//   }
+// ]
+
 //todos의 complete는 변경해야 하는 상태임으로 새로 coding
+
+let count = 0;
+
 class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: count++,
         body: "React study",
         complete: true
       },
       {
-        id: 2,
+        id: count++,
         body: "Redux study",
         complete: false
       }
-    ]
+    ],
+    newTodoBody: ""
+  };
+
+  handleInputChange = e => {
+    this.setState({
+      newTodoBody: e.target.value
+    });
+  };
+
+  handleBtnClick = e => {
+    if (this.state.newTodoBody) {
+      const newTodo = {
+        body: this.state.newTodoBody,
+        complete: false,
+        id: count++
+      };
+      this.setState({
+        todos: [...this.state.todos, newTodo],
+        newTodoBody: ""
+      });
+    }
   };
   render() {
-    const { todos } = this.state;
+    const { todos, newTodoBody } = this.state;
     return (
       <div>
         <h1>TodoList</h1>
+        <label>
+          새 할일
+          <input
+            type="text"
+            value={newTodoBody}
+            onChange={this.handleInputChange}
+          />
+          <button onClick={this.handleBtnClick}>추가</button>
+        </label>
         <ul>
           {todos.map(todo => (
             <li className={todo.complete ? "complete" : ""} key={todo.id}>
