0b3c5ed2477b23a346ea36b8a05a0192d08d5ba5
DongbeenSeo <puus1021@gmail.com>
2018-06-19 15:25:52 +0900

    user onmount component

--- src/containers/LoginFormContainer.js
+++ src/containers/LoginFormContainer.js
@@ -1,22 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 
 import LoginForm from "../components/LoginForm";
 import { UserConsumer } from "../contexts/UserContext";
 import { PageConsumer } from "../contexts/PageContext";
+import OnMount from "../components/OnMount.js";
 
-export default class LoginFormContainer extends React.Component {
+export default class LoginFormContainer extends Component {
   render() {
     return (
       <UserConsumer>
         {({ login }) => (
           <PageConsumer>
             {({ gotoTodoPage }) => (
-              <LoginForm
-                onLogin={async (username, password) => {
-                  await login(username, password);
-                  gotoTodoPage();
-                }}
-              />
+              <React.Fragment>
+                <LoginForm
+                  onLogin={async (username, password) => {
+                    await login(username, password);
+                    gotoTodoPage();
+                  }}
+                />
+                {localStorage.getItem("token") && (
+                  <OnMount onMount={gotoTodoPage} />
+                )}
+              </React.Fragment>
             )}
           </PageConsumer>
         )}
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,25 +1,8 @@
 import React, { Component } from "react";
-import LoginForm from "../components/LoginForm.js";
-import { UserConsumer } from "../contexts/UserContext.js";
-import { PageConsumer } from "../contexts/PageContext.js";
+import LoginFormContainer from "../containers/LoginFormContainer.js";
 
 export default class loginPage extends Component {
   render() {
-    return (
-      <UserConsumer>
-        {({ login }) => (
-          <PageConsumer>
-            {({ gotoTodoPage }) => (
-              <LoginForm
-                onLogin={async (username, password) => {
-                  await login(username, password);
-                  gotoTodoPage();
-                }}
-              />
-            )}
-          </PageConsumer>
-        )}
-      </UserConsumer>
-    );
+    return <LoginFormContainer />;
   }
 }
